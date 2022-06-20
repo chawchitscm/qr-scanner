56c58bd13879a0f25128819618f1e7363adf7dbe
Chaw Chit Su Thwe <scm.chawchitsuthwe@gmail.com>
2022-06-20 12:05:36 +0630

    refactor code and fix design

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// not working for barcode
+/*
+var qrFormatsToSupport = [ 
+    Html5QrcodeSupportedFormats.QR_CODE
+];
+var html5qrcodeConfig = {
+    formatsToSupport: qrFormatsToSupport,
+    experimentalFeatures: {
+        useBarCodeDetectorIfSupported: true 
+     }
+};
+const html5QrCode = new Html5Qrcode("reader", html5qrcodeConfig);
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    var scanned = document.getElementById("scanned");
+    scanned.innerText = decodedText;
+};
+const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+// If you want to prefer front camera
+// html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
+
+// If you want to prefer back camera
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
+// Select front camera or fail with `OverconstrainedError`.
+// html5QrCode.start({ facingMode: { exact: "user"} }, config, qrCodeSuccessCallback);
+
+// // Select back camera or fail with `OverconstrainedError`.
+// html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
+ 
+*/
+
+function onScanSuccess(qrCodeMessage) { 
+    var scanned = document.getElementById("scanned");
+    scanned.innerText = qrCodeMessage;
+}
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
