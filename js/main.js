04c47a04ec34d940c0061f60d1077c56fb2eb5c0
Chaw Chit Su Thwe <scm.chawchitsuthwe@gmail.com>
2022-06-22 08:09:51 +0630

    implement with quaggajs

--- js/main.js
+++ js/main.js
@@ -15,21 +15,17 @@ const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     scanned.innerText = decodedText;
 };
 const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-
 // If you want to prefer front camera
 // html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
-
 // If you want to prefer back camera
 html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
-
 // Select front camera or fail with `OverconstrainedError`.
 // html5QrCode.start({ facingMode: { exact: "user"} }, config, qrCodeSuccessCallback);
-
 // // Select back camera or fail with `OverconstrainedError`.
 // html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
  
 */
-
+/*
 function onScanSuccess(qrCodeMessage) {
     var scanned = document.getElementById("scanned");
     scanned.innerText = qrCodeMessage;
@@ -46,3 +42,119 @@ reader.firstChild.style.display = 'none';
 
 var image_link = document.getElementById("reader__dashboard_section_swaplink");
 image_link.style.color = '#000';
+
+*/
+
+var _scannerIsRunning = false;
+
+function startScanner() {
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector('#scanner-container'),
+            constraints: {
+                width: 480,
+                height: 320,
+                facingMode: "environment"
+            },
+        },
+        decoder: {
+            readers: [
+                "code_128_reader",
+                "ean_reader",
+                "ean_8_reader",
+                "code_39_reader",
+                "code_93_reader",
+                "upc_reader",
+                "upc_e_reader"
+            ],
+            multiple: false,
+            debug: {
+                showCanvas: true,
+                showPatches: true,
+                showFoundPatches: true,
+                showSkeleton: true,
+                showLabels: true,
+                showPatchLabels: true,
+                showRemainingPatchLabels: true,
+                boxFromPatches: {
+                    showTransformed: true,
+                    showTransformedBox: true,
+                    showBB: true
+                }
+            }
+        },
+
+    }, function (err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+
+        // Set flag to is running
+        _scannerIsRunning = true;
+    });
+
+    Quagga.onProcessed(function (result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {
+                        x: 0,
+                        y: 1
+                    }, drawingCtx, {
+                        color: "green",
+                        lineWidth: 2
+                    });
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {
+                    x: 0,
+                    y: 1
+                }, drawingCtx, {
+                    color: "#00F",
+                    lineWidth: 2
+                });
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {
+                    x: 'x',
+                    y: 'y'
+                }, drawingCtx, {
+                    color: 'red',
+                    lineWidth: 3
+                });
+            }
+        }
+    });
+
+
+    Quagga.onDetected(function (result) {
+        var scanned = document.getElementById("scanned");
+        scanned.innerText = result.codeResult.code;
+    });
+}
+
+
+// Start/stop scanner
+document.getElementById("btn").addEventListener("click", function () {
+    if (_scannerIsRunning) {
+        Quagga.stop();
+    } else {
+        startScanner();
+    }
+    _scannerIsRunning = !_scannerIsRunning;
+}, false);
